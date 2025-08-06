f122dff7b970e7d0aca60723e3fda3866ac0b9fd
zjohnsan <johnel21@outlook.com>
2025-08-05 21:19:10 -0300

    Update calculadora.mjs

--- calculadora.mjs
+++ calculadora.mjs
@@ -1,26 +1,26 @@
 export class Calculadora {
   sumar(a, b) {
-    return a + b;
+    return a + b
   }
 
   restar(a, b) {
-    return a - b;
+    return a - b
   }
 
   multiplicar(a, b) {
-    return a * b;
+    return a * b
   }
 
   dividir(a, b) {
-    if (b === 0) throw new Error("No se puede dividir por cero");
-    return a / b;
+    if (b === 0) console.log("No se puede dividir por cero");
+    return a / b
   }
 
   resto(a, b) {
-    return a % b;
+    return a % b
   }
 
   potencia(a, b) {
-    return Math.pow(a, b);
+    return Math.pow(a, b)
   }
 }
